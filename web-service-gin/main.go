46544b08f17d32f6054a3fab8c7e58a5535ccb63
dai <guy1nthecha1r2049@gmail.com>
2022-06-26 17:53:44 +0900

    dockerize

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -1,34 +1,33 @@
 package main
 
 import (
-    "net/http"
+	"net/http"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 // album represents data about a record album.
 type album struct {
-	ID string `json:"id"`
-	Title string `json:"title"`
-	Artist string `json:"artist"`
-	Price float64 `json:"price"`
-
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
 }
 
 // albums slice to seed record album data.
 var albums = []album{
-    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
 	router.POST("/albums", postAlbums)
-    router.GET("/albums/:id", getAlbumById)
+	router.GET("/albums/:id", getAlbumById)
 
-	router.Run("localhost:8081")
+	router.Run("0.0.0.0:8081")
 }
 
 // getAlbums responds with the list of all albums as JSON.
@@ -42,7 +41,7 @@ func postAlbums(c *gin.Context) {
 	var newAlbum album
 
 	// Call BindJSON to bind the received JSON to
-    // newAlbum.
+	// newAlbum.
 	if err := c.BindJSON(&newAlbum); err != nil {
 		return
 	}
@@ -57,14 +56,13 @@ func getAlbumById(c *gin.Context) {
 	id := c.Param("id")
 
 	// Loop over the list of albums, looking for
-    // an album whose ID value matches the parameter.
-    for _, a := range albums {
-        if a.ID == id {
-            c.IndentedJSON(http.StatusOK, a)
-            return
-        }
+	// an album whose ID value matches the parameter.
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
 	}
 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 
 }
-
